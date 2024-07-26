adb21963c919b0cc7a401addbb62e63c04be0350
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-07-26 21:59:56 +0800

    fix(lint):
    ESLint globals for web extensions

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import antfu from "@antfu/eslint-config"
 import unocss from "@unocss/eslint-config/flat"
+import globals from "globals"
 
 export default antfu({
   vue: true,
   stylistic: {
     quotes: "double",
+  },
+  languageOptions: {
+    globals: {
+      ...globals.webextensions,
+      ...globals.browser,
+    },
   },
 }, unocss)
